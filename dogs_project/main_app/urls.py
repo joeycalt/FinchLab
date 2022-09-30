debb5e420be148d4112b2d9293178086663df862
joeycalt <joeycalt@yahoo.com>
2022-09-30 16:26:34 -0400

    got the views, but getting an error when using context in views folder

---
+++ dogs_project/main_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.Home.as_view(), name="home"),
+    path('about/', views.About.as_view(), name="about"),
+    path('dogs/', views.DogsList.as_view(), name="dogs_list")
+]
